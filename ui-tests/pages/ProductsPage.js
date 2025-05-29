a538cbbae5a6cbb70cd140310661007a0db8d1e4
Ashik Rabbani <ashik.ashik.rabbni@gmail.com>
2025-05-29 18:21:45 +0600

    refactored productsPAge

--- ui-tests/pages/ProductsPage.js
+++ ui-tests/pages/ProductsPage.js
@@ -2,16 +2,6 @@ export class ProductsPage {
     getAllProducts() {
       return cy.get('.inventory_item');
     }
-  
-    clickAddToCart(productName,productPrice) {
-      cy.contains('.inventory_item', productName)
-        .contains('Add to cart')
-        .click();
-    }
-  
-    goToCart() {
-      cy.get('.shopping_cart_link').click();
-    }
   }
   
   export const productsPage = new ProductsPage();
