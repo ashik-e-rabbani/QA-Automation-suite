8a0958e511b596b4939f89dd9fcd7eb22572308c
Ashik Rabbani <ashik.ashik.rabbni@gmail.com>
2025-05-29 23:33:01 +0600

    refactored config

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,28 @@ const { defineConfig } = require("cypress");
 require('dotenv').config();
 
 module.exports = defineConfig({
-  defaultCommandTimeout:15000,
+  defaultCommandTimeout: 15000,
   pageLoadTimeout: 60000,
-  requestTimeout: 5000,
+  requestTimeout: 10000,
+  // reporter: 'mochawesome',
+  // reporterOptions: {
+  //   reportDir: 'ui-tests/reports',
+  //   overwrite: true,
+  //   html: true,
+  //   json: true,
+  // },
+
   env: {
     username: process.env.CYPRESS_username || 'standard_user',
-    password: process.env.CYPRESS_password || 'secret_sauce',
-    baseUrl: process.env.CYPRESS_baseUrl || 'https://www.saucedemo.com'
+    password: process.env.CYPRESS_password || 'secret_sauce'
+
   },
+
   e2e: {
     specPattern: 'ui-tests/e2e/**/*.cy.{js,ts}',
     supportFile: 'ui-tests/support/e2e.js',
     fixturesFolder: 'ui-tests/fixtures',
-    baseUrl: 'https://www.saucedemo.com/',
+    baseUrl: process.env.CYPRESS_baseUrl || 'https://www.saucedemo.com',
     screenshotsFolder: 'ui-tests/screenshots',
     videosFolder: 'ui-tests/videos',
     setupNodeEvents(on, config) {
